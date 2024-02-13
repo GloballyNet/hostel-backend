d8df1e13427b8841df6104102a7f4ea24280cd51
Emmanuel Koduah Tweneboah <emmanueltweneboah71@gmail.com>
2024-02-13 08:49:33 +0100

    added and admin entity

---
+++ src/admin/entities/admin.entity.ts
@@ -0,0 +1,35 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Admin{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({length: 100})
+    firstName: string;
+
+    @Column({length: 100})
+    lastName: string;
+
+    
+    @Column({unique: true})
+    email: string;
+
+    @Column()
+    dateOfBirth: Date;
+
+    @Column()
+    phone: string;
+
+    @Column()
+    address: string;
+
+    @Column()
+    created_at: Date;
+
+    @Column()
+    updated_at: Date;
+
+    @Column()
+    deleted_at: Date;
+}
