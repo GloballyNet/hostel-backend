2b9dbedf9e95f688189fea3f51ebec9f63c20658
prabhjot <sprabhjot768@gmail.com>
2024-02-15 00:22:06 +0530

    done changes

--- src/db/data.source.ts
+++ src/db/data.source.ts
@@ -1,17 +1,17 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 
 export const dbdatasource: DataSourceOptions = {
-  type: process.env.DB_TYPE as any,
-  host: process.env.PG_HOST,
-  port: parseInt(process.env.PG_PORT),
-  username: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DB,
-  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'postgres',
+  entities: ['dist/**/*.entity.js'],
   synchronize: true,
   // Migration path
-  migrations: ['build/task/migrations/*.js'],
-  migrationsTableName: 'migrarions',
+  migrations: ['dist/db/migrations/*.js'],
+  migrationsTableName: 'migrations',
 };
 
 const dataSource = new DataSource(dbdatasource);
