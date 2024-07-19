13a215ab073aa87b0a69bf64d4656b723acc232d
Tkayy729 <emmanueltweneboah71@gmail.com>
2024-07-19 02:36:19 +0200

    added the providers and controllers for room

--- src/room/room.module.ts
+++ src/room/room.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { Room } from '../entites/room.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { RoomService } from './room.service';
+import { RoomRepository } from './room.repository';
+import { RoomController } from './room.controller';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Room])],
+  imports: [TypeOrmModule.forFeature([Room, RoomRepository])],
+  providers: [
+    RoomService,
+    {
+      provide: 'RoomRepository',
+      useFactory: (dataSource: DataSource) => new RoomRepository(dataSource),
+      inject: [DataSource],
+    },
+  ],
+  controllers: [RoomController],
+  exports: [],
 })
 export class RoomModule {}
