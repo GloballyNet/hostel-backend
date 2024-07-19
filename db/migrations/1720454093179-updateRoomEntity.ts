35eb94a9af95e51e9b3b688cba4ce59246a5d647
Tkayy729 <emmanueltweneboah71@gmail.com>
2024-07-19 02:35:11 +0200

    updated room entity migration

---
+++ db/migrations/1720454093179-updateRoomEntity.ts
@@ -0,0 +1,44 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+export class UpdateRoomEntity1620309249000 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.renameColumn('room', 'status', 'available');
+    await queryRunner.dropColumn('room', 'hostelId');
+    await queryRunner.addColumn(
+      'room',
+      new TableColumn({
+        type: 'int',
+        name: 'capacity',
+      }),
+    );
+    await queryRunner.changeColumn(
+      'room',
+      'rommType',
+      new TableColumn({
+        name: 'roomType',
+        type: 'enum',
+        enum: ['SINGLE', 'DOUBLE', 'SUITE'],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.changeColumn(
+      'room',
+      'roomType',
+      new TableColumn({
+        name: 'roomType',
+        type: 'varchar',
+      }),
+    );
+    await queryRunner.addColumn(
+      'room',
+      new TableColumn({
+        name: 'hostelId',
+        type: 'int',
+      }),
+    );
+    await queryRunner.dropColumn('room', 'capacity');
+    await queryRunner.renameColumn('room', 'available', 'status');
+  }
+}
