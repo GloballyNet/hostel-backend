b52e759c4b385a5477573294dee0348759d0f9a9
Tkayy729 <emmanueltweneboah71@gmail.com>
2024-07-25 03:15:02 +0200

    added a response entity

--- src/room/room.dto.ts
+++ src/room/room.dto.ts
@@ -1,18 +1,46 @@
+import { AutoMap } from "@automapper/classes";
+import { ApiProperty } from "@nestjs/swagger";
 import { IsBoolean, IsInt, IsString, Max } from "class-validator";
-import { RoomType } from "src/entites/room.entity";
+import { RoomType } from "src/enums/room.enum";
 
 export class CreateRoomDto {
     @IsInt() 
+    @ApiProperty({ description: 'The number given the room' })
     readonly roomNumber: number;
     @IsString()
     @Max(256)
+    @ApiProperty({ description: 'The description of the room' })
     readonly description: string;
     @Max(10)
     @IsInt()
+    @ApiProperty({ description: 'The capacity of the room' })
     readonly capacity: number;
     @IsBoolean()
+    @ApiProperty({ description: 'The availability of the room' })
     readonly available: boolean;
     @IsString()
+    @ApiProperty({ description: 'The type of room' })
+    readonly roomType: RoomType;
+}
+
+export class RoomResponseDto {
+    @ApiProperty({ description: 'The id of the room' })
+    @AutoMap()
+    readonly id: number;
+    @AutoMap()
+    @ApiProperty({ description: 'The number given the room' })
+    readonly roomNumber: number;
+    @AutoMap()
+    @ApiProperty({ description: 'The description of the room' })
+    readonly description: string;
+    @AutoMap()
+    @ApiProperty({ description: 'The capacity of the room' })
+    readonly capacity: number;
+    @AutoMap()
+    @ApiProperty({ description: 'The availability of the room' })
+    readonly available: boolean;
+    @AutoMap()
+    @ApiProperty({ description: 'The type of room' })
     readonly roomType: RoomType;
 }
   
