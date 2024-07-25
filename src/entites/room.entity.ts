722337884875b3552e1b69c392326a87648e645d
Tkayy729 <emmanueltweneboah71@gmail.com>
2024-07-25 03:14:44 +0200

    removed unnecessary spaces

--- src/entites/room.entity.ts
+++ src/entites/room.entity.ts
@@ -1,45 +1,40 @@
+import { AutoMap } from '@automapper/classes';
 import { Booking } from 'src/entites/booking.entity';
 import { Student } from 'src/entites/student.entity';
+import { RoomType } from 'src/enums/room.enum';
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
-
-export enum RoomType {
-  SINGLE = 'SINGLE',
-  DOUBLE = 'DOUBLE',
-  SUITE = 'SUITE',
-}
 
 @Entity()
 export class Room {
+  @AutoMap()
   @PrimaryGeneratedColumn()
   id: number;
-
   @Column()
   roomNumber: number;
-
+  @AutoMap()
   @Column()
   capacity: number;
-
   @Column({
     type: 'enum',
     enum: RoomType,
   })
   roomType: RoomType;
-
+  @AutoMap()
   @Column()
   available: boolean;
-
+  @AutoMap()
   @Column()
   description: string;
-
+  @AutoMap()
   @Column()
   created_at: Date;
-
+  @AutoMap()
   @Column()
   updated_at: Date;
-
+  @AutoMap()
   @OneToMany(() => Student, (student) => student.room)
   students: Student[];
-
+  @AutoMap()
   @OneToMany(() => Booking, (booking) => booking.room)
   bookings: Booking[];
 }
