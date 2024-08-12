f2b3998784e4adabc2e2e2c2c0f6bfd3d558036c
Tkayy729 <emmanueltweneboah71@gmail.com>
2024-08-12 04:02:52 +0200

    role column added to admin

--- src/entites/admin.entity.ts
+++ src/entites/admin.entity.ts
@@ -4,31 +4,24 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 export class Admin {
   @PrimaryGeneratedColumn()
   id: number;
-
   @Column({ length: 100 })
   firstName: string;
-
   @Column({ length: 100 })
   lastName: string;
-
   @Column({ unique: true })
   email: string;
-
   @Column()
   dateOfBirth: Date;
-
   @Column()
   phone: string;
-
   @Column()
   address: string;
-
   @Column()
   created_at: Date;
-
   @Column()
   updated_at: Date;
-
   @Column()
   deleted_at: Date;
+  @Column({ default: 'admin' })
+  role: string;
 }
