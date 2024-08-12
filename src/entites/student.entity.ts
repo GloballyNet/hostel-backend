e12be094cfa308e44119bff6b025a15159415105
Tkayy729 <emmanueltweneboah71@gmail.com>
2024-08-12 04:03:02 +0200

    role column added to student

--- src/entites/student.entity.ts
+++ src/entites/student.entity.ts
@@ -15,46 +15,35 @@ import { Complaint } from './complaint.entity';
 export class Student {
   @PrimaryGeneratedColumn()
   id: number;
-
   @Column({ length: 100 })
   firstName: string;
-
   @Column({ length: 100 })
   lastName: string;
-
   @Column()
   age: number;
-
   @Column({ unique: true })
   email: string;
-
   @Column()
   dateOfBirth: Date;
-
   @Column()
   phone: string;
-
   @Column()
   address: string;
-
   @Column()
   created_at: Date;
-
   @Column()
   updated_at: Date;
-
   @Column()
   @Column({ nullable: true })
   deleted_at: Date;
-
   @JoinColumn()
   @ManyToOne(() => Room, (room) => room.students)
   room: Room;
-
   @OneToOne(() => Booking, { nullable: true })
   @JoinColumn()
   booking: Booking;
-
   @OneToMany(() => Complaint, (complaint) => complaint.student)
   complaints: Complaint[];
+  @Column({ default: 'student' })
+  role: string;
 }
